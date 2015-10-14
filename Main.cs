accbc7936ae962a4534346d0a96db73095298ca0
Luuk Holleman <luukholleman@gmail.com>
2015-10-14 20:20:19 +0200

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Custom_Scenery
+{
+    class Main : IMod
+    {
+        private GameObject _go;
+
+        public void onEnabled()
+        {
+            _go = new GameObject();
+
+            _go.AddComponent<SceneryLoader>();
+
+            _go.GetComponent<SceneryLoader>().LoadScenery();
+        }
+
+        public void onDisabled()
+        {
+            _go.GetComponent<SceneryLoader>().UnloadScenery();
+
+            Object.Destroy(_go);
+        }
+
+        public string Name { get { return "Custom Scenery"; } }
+        public string Description { get { return "Custom Scenery"; } }
+    }
+}
---
+++ SceneryLoader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom_Scenery
+{
+    class SceneryLoader : MonoBehaviour
+    {
+        private List<Deco> _sceneryObjects = new List<Deco>(); 
+
+        private List<string> _scenery = new List<string>()
+        {
+            "Bush1",
+            "Bush2",
+            "Bush3",
+            "Bush4",
+            "Bush5",
+            "Bush6",
+            "Bush7",
+            "Fern",
+            "RockMesh",
+            "Alder",
+            "Bamboo",
+            "BananaPlant",
+            "Banyan",
+            "JapaneseMaple",
+            "Mimosa",
+            "Palm",
+            "Palm (group)",
+            "ScotsPineTypeA",
+            "Sycamore",
+            "ThinTree",
+            "Willow",
+        };
+
+        public void LoadScenery()
+        {
+            using (WWW www = new WWW("file://" + Application.streamingAssetsPath + "/mods/custom-scenery/scenery"))
+            {
+                if (www.error != null)
+                    throw new Exception("Download had an error:" + www.error);
+
+                AssetBundle bundle = www.assetBundle;
+
+                foreach (string s in _scenery)
+                {
+                    GameObject scenery = Instantiate(bundle.LoadAsset(s)) as GameObject;
+
+                    Deco d = scenery.AddComponent<Deco>();
+
+                    d.price = 100;
+
+                    ScriptableSingleton<AssetManager>.Instance.registerObject(d);
+
+                    _sceneryObjects.Add(d);
+                }
+
+                bundle.Unload(false);
+            }
+        }
+
+        public void UnloadScenery()
+        {
+            foreach (Deco deco in _sceneryObjects)
+            {
+                ScriptableSingleton<AssetManager>.Instance.unregisterObject(deco);
+            }
+        }
+    }
+}
